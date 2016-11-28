586a3097f0504424038fe2b7089ad1b3afb8ea66
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2016-11-28 12:26:10 -0600

    scheduler final

--- scheduler/start_flow.sh
+++ scheduler/start_flow.sh
@@ -1,28 +1,48 @@
 #!/bin/bash
 
-#start flume
-nohup /Users/vishnu/apache-flume-1.6.0-bin/bin/flume-ng agent -n TwitterAgent -c /Users/vishnu/apache-flume-1.6.0-bin/conf -f /Users/vishnu/apache-flume-1.6.0-bin/conf/twitter-conf.properties -Dflume.root.logger=INFO,console&
+#variables
+job_jar=/Users/vishnu/git/hadoop_datascience/MapreduceNew/target/examples-1.0-SNAPSHOT-jar-with-dependencies.jar
+flume_home=/Users/vishnu/apache-flume-1.6.0-bin
+mrinput_dir=/adbms/wordcloud/mrinput/
+mroutput_dir=/adbms/wordcloud/mroutput/
+tweets_dir=/adbms/wordcloud/tweets/
+hive_table_dir=/adbms/wordcloud/hive_external_table
+#directory for storing processed tweet files.
+hist_dir=/adbms/wordcloud/hist/
+
+#create required hdfs folder
+hadoop fs -mkdir $mrinput_dir
+hadoop fs -mkdir $hist_dir
+
+#start flume.
+nohup $flume_home/bin/flume-ng agent -n agent -c $flume_home/conf -f $flume_home/conf/flume-conf.properties -Dflume.root.logger=INFO,console&
 flume_pid=$!
-echo flume_pid=$flume_pid
+echo "flume started with pid"=$flume_pid
 
 #wait 15minutes
-sleep 15m
+echo "sleeping for 10 minutes"
+sleep 600
+echo "wokeup"
+echo "killing the flume process"
+kill $flume_pid
 
-#kill flume
-kill -9 $flume_pid
 
 #verify flume is killed
-if [ -n "$(ps -p $flume_pid -o pid=)" ]
+#if [ -n "$(ps -p $flume_pid -o pid=)" ]
 	#wait for 5 seconds and check again
-	sleep 5s
-	if [ -n "$(ps -p $flume_pid -o pid=)" ]
-		echo "could not kill flume process, program exiting"
-		exit
+	#sleep 5s
+	#if [ -n "$(ps -p $flume_pid -o pid=)" ]
+		#echo "could not kill flume process, program exiting"
+		#exit
 
 #move flume result to a working dir, except .tmp files
-hadoop fs -mv /adbms/wordcloud/tweets/*.log /adbms/wordcloud/mrinput/
+echo "moving log files to working directory"
+hadoop fs -mv $mrinput_dir* $hist_dir
+wait
+hadoop fs -mv $tweets_dir/*.log $mrinput_dir
 wait
 
-nohup hadoop jar examples-1.0-SNAPSHOT.jar WordCount /adbms/wordcloud/mrinput/ /adbms/wordcloud/hive_external_table &
-hadoop_pid =$!
-echo hadoop_job_pid=$hadoop_pid
+hadoop fs -rmr $mroutput_dir
+nohup hadoop jar $job_jar WordCount $mrinput_dir $mroutput_dir &
+hadoop_pid=$!
+echo "hadoop job started hadoop_job_pid"=$hadoop_pid
