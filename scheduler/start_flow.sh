03ba282e943d9b9c6b2bfa335c5fe8c44e2c4bc8
Vishnu Viswanath <vishnu.viswanath25@gmail.com>
2016-12-01 12:13:39 -0600

    update shellscript and move r to folder

--- scheduler/start_flow.sh
+++ scheduler/start_flow.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #variables
-job_jar=/Users/vishnu/git/hadoop_datascience/MapreduceNew/target/examples-1.0-SNAPSHOT-jar-with-dependencies.jar
+job_jar=/Users/vishnu/temp/wordcount/target/wordcount-1.0-SNAPSHOT.jar
 flume_home=/Users/vishnu/apache-flume-1.6.0-bin
-mrinput_dir=/adbms/wordcloud/mrinput/
-mroutput_dir=/adbms/wordcloud/mroutput/
 tweets_dir=/adbms/wordcloud/tweets/
-hive_table_dir=/adbms/wordcloud/hive_external_table
+mrinput_dir=/adbms/wordcloud/mrinput/
+mroutput_dir=/adbms/wordcloud/mroutput
+hive_table_dir=/user/hive/warehouse/tweets_count/
 #directory for storing processed tweet files.
 hist_dir=/adbms/wordcloud/hist/
 
@@ -15,11 +15,11 @@ hadoop fs -mkdir $mrinput_dir
 hadoop fs -mkdir $hist_dir
 
 #start flume.
-nohup $flume_home/bin/flume-ng agent -n agent -c $flume_home/conf -f $flume_home/conf/flume-conf.properties -Dflume.root.logger=INFO,console&
+####nohup $flume_home/bin/flume-ng agent -n agent -c $flume_home/conf -f $flume_home/conf/flume-conf.properties -Dflume.root.logger=INFO,console&
 flume_pid=$!
 echo "flume started with pid"=$flume_pid
 
-#wait 15minutes
+#wait 10minutes
 echo "sleeping for 10 minutes"
 sleep 600
 echo "wokeup"
@@ -37,12 +37,14 @@ kill $flume_pid
 
 #move flume result to a working dir, except .tmp files
 echo "moving log files to working directory"
-hadoop fs -mv $mrinput_dir* $hist_dir
+hadoop fs -mv $mrinput_dir*.log $hist_dir
 wait
 hadoop fs -mv $tweets_dir/*.log $mrinput_dir
 wait
 
 hadoop fs -rmr $mroutput_dir
-nohup hadoop jar $job_jar WordCount $mrinput_dir $mroutput_dir &
+hadoop jar $job_jar $mrinput_dir $mroutput_dir
 hadoop_pid=$!
-echo "hadoop job started hadoop_job_pid"=$hadoop_pid
+wait
+name=$(($(date +%s)))
+hadoop fs -mv $mroutput_dir/part* $hive_table_dir/$name
